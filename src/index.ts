f471881718e588a350edac445a031e6c42019e77
GonzaRomera <92063887+GonzaRomera@users.noreply.github.com>
2021-11-07 22:03:11 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+/*let numerosA: number[] = new Array(6);
+let numerosB: number[] = new Array(6);
+let suma: number[] = new Array(6);
+let i: number;
+
+for (i = 0; i < 6; i++) {
+  numerosA[i] = Number(prompt("ingresar numerosA"));
+}
+
+for (i = 0; i < 6; i++) {
+  numerosB[i] = Number(prompt("ingresar numerosB"));
+}
+
+for (i = 0; i < 6; i++) {
+  suma[i] = numerosA[i] + numerosB[i];
+  console.log("el resultado es: ", suma[i])
+}
+*/
